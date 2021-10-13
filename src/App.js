1f9d61aa5eeab735b8be93a65403ebb868f0ae4b
Xuan <xzhang@grubhub.com>
2021-10-13 17:20:55 -0500

    Only show app in mobile browser

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { Swipe } from './components/Swipe';
 import FakeGrubHubApp from './components/FakeGrubHubApp';
 import { Alert } from './components/Alert';
 import SwipeContainer from "./components/SwipeContainer";
+import { isMobile } from 'react-device-detect';
 
 function App() {
   const [showSwipe, setShowSwipe] = useState(false);
   const [showGrubHubApp, setShowGrubHubApp] = useState(true);
   const [showAlert, setShowAlert] = useState(true);
+
+  if (!isMobile) {
+    return <h1>Warning: ...</h1>
+  }
 
   return (
     <div className="App">
